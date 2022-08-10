8fe67a7af84265fdbe0cd576c7d040a4a713c7e0
Carly Richardson <cbrich16@gmail.com>
2022-08-09 20:46:10 -0500

    updated files

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ from crypt import methods
 from flask import Flask, url_for, render_template, redirect, json, jsonify, request
 from flask_pymongo import PyMongo
 import random
-import requests
 
 import os
 
 app = Flask(__name__)
-
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-mongo = PyMongo(app)
 
 @app.route("/")
 def home():
@@ -30,13 +26,6 @@ def goto_code():
 @app.route("/bios", methods=["GET", "POST"])
 def goto_bios():
     return(request.form)
-
-def load_and_get_stats(body: dict) -> dict:
-    chars = mongo.db.collectionname
-
-    chars.update_one({body}. {'$inc':{'displayed': 1}})
-
-    return chars.find()
 
 if __name__ == "__main__":
     app.run(debug=True)
