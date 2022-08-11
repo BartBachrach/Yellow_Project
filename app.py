3130d28627e008eef2a4cccb1f63ee6823c185f5
Carly Richardson <cbrich16@gmail.com>
2022-08-10 20:52:28 -0500

    Edited files; tried to connect flask to AWS.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from crypt import methods
 from flask import Flask, url_for, render_template, redirect, json, jsonify, request
 from flask_pymongo import PyMongo
+import pymysql
 import random
 
 import os
 
+from config import awskey
+
 app = Flask(__name__)
+
+
+# conn = pymysql.connect(
+#         host= 'movie-project.cc10tszik781.us-east-1.rds.amazonaws.com', 
+#         port = 5432,
+#         user = 'postgres', 
+#         password = awskey,
+#         db = 'movie-project',
+#         )
 
 @app.route("/")
 def home():
@@ -13,19 +25,20 @@ def home():
 
 @app.route("/recs", methods=["GET", "POST"])
 def get_recs():
-    return(request.form)
+
+    return jsonify('sample_table')
 
 @app.route("/about", methods=["GET", "POST"])
 def goto_about():
-    return(request.form)
+    return render_template('about.html')
 
 @app.route("/code", methods=["GET", "POST"])
 def goto_code():
-    return(request.form)
+    return render_template('code.html')
 
 @app.route("/bios", methods=["GET", "POST"])
 def goto_bios():
-    return(request.form)
+    return render_template('bios.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+awskey = "Yellow#1"
