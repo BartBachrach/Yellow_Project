42879824a7d6db2e555bffcdbc00c664aa9ac084
Carly Richardson <cbrich16@gmail.com>
2022-08-11 21:42:03 -0500

    Updated files and connect app to db.

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 from crypt import methods
 from flask import Flask, url_for, render_template, redirect, json, jsonify, request
-from flask_pymongo import PyMongo
-import pymysql
 import random
-
 import os
 
-from config import awskey
+#from config import awskey
+
+import psycopg2
+
+conn = psycopg2.connect(
+        host="movie-project.cc10tszik781.us-east-1.rds.amazonaws.com",
+        database="movies",
+        user="#",
+        password="#")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
 
 app = Flask(__name__)
-
-
-# conn = pymysql.connect(
-#         host= 'movie-project.cc10tszik781.us-east-1.rds.amazonaws.com', 
-#         port = 5432,
-#         user = 'postgres', 
-#         password = awskey,
-#         db = 'movie-project',
-#         )
 
 @app.route("/")
 def home():
@@ -25,6 +24,29 @@ def home():
 
 @app.route("/recs", methods=["GET", "POST"])
 def get_recs():
+    query = '''SELECT * FROM sample_table;
+    '''
+
+    # IF exampleSelect1 = Science Fiction, filter table to * WHERE class = 1
+        # exampleSelect1 = Suspenseful, filter to * WHERE class = 0
+        # exampleSelect1 = Animated, filter to * WHERE class = 2
+        # exampleSelect1 = Critically Acclaimed, filter to * WHERE class = 4
+        # exampleSelect1 = I'm feeling lucky!, filter to * WHERE class = 3
+
+    # THEN IF exampleSelect2 = 2022 - 2010, filter to * WHERE year >=2010
+                #exampleSelect2 = 2010 - 2000, filter to * WHERE year <= 2010 and >= 2000
+                #exampleSelect2 = 2000 - 1980, filter to * WHERE year <= 2000 and >= 1980
+                #exampleSelect2 = 1980 - 1960, filter to * WHERE year <= 1980 and >= 1960
+                #exampleSelect2 = 1959 or older, filter to * WHERE year <= 1959
+    
+    # THEN IF exampleSelect3 = Yes - Show me only 4 stars and above.
+                # exampleSelect3 = A little bit - Show me only 3 stars and above.
+                # exampleSelect3 = No - Show me anything.     
+
+    cur.execute(query)
+    data = cur.fetchall()
+    for sample in data:
+        print(sample)
 
     return (request.form)
 
